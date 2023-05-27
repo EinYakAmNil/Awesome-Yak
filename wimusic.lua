c2e76374f84009314235e16b8928685995f24e4f
EinYakAmNil <einyak@gmail.com>
2023-05-28 00:57:48 +0200

    use unified widget shell script

--- wimusic.lua
+++ wimusic.lua
@@ -1,13 +1,7 @@
-local naughty = require("naughty")
-local beautiful = require("beautiful")
 local awful = require("awful")
 
-local function debug(string)
-	naughty.notify({ title = string })
-end
-
-local s = awful.screen.focused()
-awful.spawn.easy_async("Musik", function(out)
-	music_display.text = out .. " "
-	music_display.forced_width = music_display:get_preferred_size(1) + 50
+awful.spawn.easy_async("wimusic status", function(out)
+	-- local song = out 
+	music_display.text = "𝅘𝅥𝅮♪  " .. out
+	music_display.forced_width = music_display:get_preferred_size(1) + 20
 end)
