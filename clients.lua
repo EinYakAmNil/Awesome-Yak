5c43b63fa35a53eb4418cee42b4be9e4f5040116
EinYakAmNil <einyak@gmail.com>
2023-06-16 09:55:22 +0200

    refactoring client and cursor rules

--- clients.lua
+++ clients.lua
@@ -1,4 +1,133 @@
-beautiful = require("beautiful")
+local beautiful = require("beautiful")
+local awful = require("awful")
+local cursor = require("cursor")
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+
+-- Rules to apply to new clients (through the "manage" signal).
+awful.rules.rules = {
+	-- All clients will match this rule.
+	{
+		rule = {},
+		properties = {
+			border_width = beautiful.border_width,
+			border_color = beautiful.border_normal,
+			focus = awful.client.focus.filter,
+			raise = true,
+			keys = cursor["keys"],
+			buttons = cursor["buttons"],
+			screen = awful.screen.preferred,
+			placement = awful.placement.no_overlap + awful.placement.no_offscreen,
+		},
+	},
+
+	-- No titlebars
+	{
+		rule_any = { type = { "normal", "dialog" } },
+		properties = { titlebars_enabled = false },
+	},
+
+	-- Floating clients.
+	{
+		rule_any = {
+			instance = {
+				"DTA", -- Firefox addon DownThemAll.
+				"copyq", -- Includes session name in class.
+				"pinentry",
+			},
+			class = {
+				"Arandr",
+				"Blueman-manager",
+				"Gpick",
+				"Kruler",
+				"MessageWin", -- kalarm.
+				"Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
+				"Wpa_gui",
+				"veromix",
+				"xtightvncviewer",
+			},
+			-- Note that the name property shown in xprop might be set slightly after creation of the client
+			-- and the name shown there might not match defined rules here.
+			name = {
+				"Event Tester", -- xev.
+			},
+			role = {
+				"AlarmWindow", -- Thunderbird's calendar.
+				"ConfigManager", -- Thunderbird's about:config.
+				"pop-up", -- e.g. Google Chrome's (detached) Developer Tools.
+			},
+		},
+		properties = { floating = true },
+	},
+	{
+		rule_any = { class = { "firefox", "Brave-browser" } },
+		properties = { tag = defaults.tagnames["browser"] },
+	},
+	{
+		rule_any = { class = { "Steam", "Lutris" } },
+		properties = { tag = defaults.tagnames["gameclients"] },
+	},
+	{
+		rule_any = { name = { "Steam" } },
+		properties = { tag = defaults.tagnames["gameclients"] },
+	},
+	{
+		rule_any = { class = { "mpv" } },
+		properties = { tag = defaults.tagnames["videos"] },
+	},
+	{
+		rule_any = { class = { "krita", "Gimp-2.10", "Blender" } },
+		properties = { tag = defaults.tagnames["art"] },
+	},
+	{
+		rule = { class = "Blender", name = "Blender Preferences" },
+		properties = { floating = true },
+	},
+	{
+		rule = { class = "leagueclientux.exe" },
+		properties = {
+			tag = defaults.tagnames["gameclients"],
+			floating = true,
+		},
+	},
+	{
+		rule_any = {
+			class = {
+				"steam_app_.*",
+				"league of legends.exe",
+				"ShogunShowdownPrologue.x86_64",
+				"medieval2.exe",
+				"assassinscreed_dx10.exe",
+				"retroarch",
+			},
+		},
+		properties = {
+			screen = 1,
+			tag = defaults.tagnames["games"],
+			fullscreen = true,
+		},
+	},
+	{
+		rule_any = { class = { "Signal" } },
+		properties = { screen = 2, tag = defaults.tagnames["communication"] },
+	},
+	{
+		rule_any = { class = { "riotclientservices.exe", "leagueclient.exe" } },
+		properties = { tag = defaults.tagnames["trash"] },
+	},
+}
+-- }}}
+
+-- {{{ Signals
+-- Signal function to execute when a new client appears.
+client.connect_signal("manage", function(c)
+	-- Set the windows at the slave,
+	-- i.e. put it at the end of others instead of setting it master.
+	-- if not awesome.startup then awful.client.setslave(c) end
+
+	if awesome.startup and not c.size_hints.user_position and not c.size_hints.program_position then
+		-- Prevent clients from being unreachable after screen count changes.
+		awful.placement.no_offscreen(c)
+	end
+end)
--- cursor.lua
+++ cursor.lua
@@ -1,4 +1,56 @@
+local gears = require("gears")
+local awful = require("awful")
+local M = {}
+
 -- Enable sloppy focus, so that focus follows mouse.
 client.connect_signal("mouse::enter", function(c)
 	c:emit_signal("request::activate", "mouse_enter", { raise = false })
 end)
+
+M.keys = gears.table.join(
+	awful.key({ defaults.modkey }, "f", function(c)
+		c.fullscreen = not c.fullscreen
+		c:raise()
+	end, { description = "toggle fullscreen", group = "client" }),
+	awful.key({ defaults.modkey, "Shift" }, "q", function(c)
+		c:kill()
+	end, { description = "close", group = "client" }),
+	awful.key(
+		{ defaults.modkey, "Shift" },
+		"f",
+		awful.client.floating.toggle,
+		{ description = "toggle floating", group = "client" }
+	),
+	awful.key({ defaults.modkey, "Control" }, "Return", function(c)
+		c:swap(awful.client.getmaster())
+	end, { description = "move to master", group = "client" }),
+	awful.key({ defaults.modkey }, "o", function(c)
+		c:move_to_screen()
+	end, { description = "move to screen", group = "client" }),
+	awful.key({ defaults.modkey }, "t", function(c)
+		c.ontop = not c.ontop
+	end, { description = "toggle keep on top", group = "client" }),
+	awful.key({ defaults.modkey }, "n", function(c)
+		c.minimized = true
+	end, { description = "minimize", group = "client" }),
+	awful.key({ defaults.modkey, "Control" }, "m", function(c)
+		c.maximized = not c.maximized
+		c:raise()
+	end, { description = "(un)maximize", group = "client" })
+)
+
+M.buttons = gears.table.join(
+	awful.button({}, 1, function(c)
+		c:emit_signal("request::activate", "mouse_click", { raise = true })
+	end),
+	awful.button({ defaults.modkey }, 1, function(c)
+		c:emit_signal("request::activate", "mouse_click", { raise = true })
+		awful.mouse.client.move(c)
+	end),
+	awful.button({ defaults.modkey }, 3, function(c)
+		c:emit_signal("request::activate", "mouse_click", { raise = true })
+		awful.mouse.client.resize(c)
+	end)
+)
+
+return M
